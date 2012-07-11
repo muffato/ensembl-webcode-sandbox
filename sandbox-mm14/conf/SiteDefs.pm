6d048c659a533d665753b8c928f5b15d84e94c95
Matthieu Muffato <mm14@sanger.ac.uk>
2012-07-11 13:31:36 +0000

    Sandbox now working

--- sandbox-mm14/conf/SiteDefs.pm
+++ sandbox-mm14/conf/SiteDefs.pm
@@ -2,15 +2,23 @@ use strict;
 
 package EnsEMBL::Matt::SiteDefs;
 sub update_conf {
-  $SiteDefs::ENSEMBL_PORT                   = 9030; 
-  #$SiteDefs::ENSEMBL_PROXY_PORT             = 80;
-  $SiteDefs::ENSEMBL_SERVERNAME             = 'enssand-01.internal.sanger.ac.uk';
+  $SiteDefs::ENSEMBL_PORT                   = 9030;
+  $SiteDefs::ENSEMBL_PROXY_PORT             = undef;
+  $SiteDefs::ENSEMBL_SERVERNAME             = undef;
   $SiteDefs::ENSEMBL_STATIC_SERVER          = '';
+  $SiteDefs::ENSEMBL_MIRRORS                = undef;
 
+  $SiteDefs::ENSEMBL_LOGINS                 = 0; ## Change to 1 to turn user logins on
   $SiteDefs::ENSEMBL_TMP_DIR_BLAST          = $SiteDefs::ENSEMBL_SERVERROOT."/blastqueue";
   $SiteDefs::ENSEMBL_MAIL_ERRORS            = 0;
-  $SiteDefs::ENSEMBL_USERDB_HOST            = 'ensdb-1-07.internal.sanger.ac.uk';
-  $SiteDefs::ENSEMBL_USERDB_PORT            =  5305;
+  
+  $SiteDefs::ENSEMBL_BLAST_ENABLED = 0;
+  $SiteDefs::ENSEMBL_MEMCACHED = {};
+
+  $SiteDefs::ENSEMBL_API_VERBOSITY          = 'WARNING'; ## Shut up the API a bit!
+  $SiteDefs::ENSEMBL_DEBUG_FLAGS           |= $SiteDefs::ENSEMBL_DEBUG_HANDLER_ERRORS;
+  $SiteDefs::ENSEMBL_DEBUG_FLAGS           |= $SiteDefs::ENSEMBL_DEBUG_VERBOSE_STARTUP;
+  $SiteDefs::ENSEMBL_DEBUG_FLAGS           |= $SiteDefs::ENSEMBL_DEBUG_EXTERNAL_COMMANDS;
 
   $SiteDefs::ENSEMBL_TMP_DIR                = $SiteDefs::ENSEMBL_SERVERROOT.'/tmp';
   $SiteDefs::ENSEMBL_TMP_DIR_IMG            = $SiteDefs::ENSEMBL_SERVERROOT.'/img/tmp';
@@ -28,6 +36,19 @@ sub update_conf {
   $SiteDefs::ENSEMBL_PIDFILE    = "$LOG.httpd.pid";
   $SiteDefs::ENSEMBL_ERRORLOG  = "$LOG$DATESTAMP.error_log";
   $SiteDefs::ENSEMBL_CUSTOMLOG = "$LOG$DATESTAMP.access_log ensembl_extended";
+
+  $SiteDefs::ENSEMBL_USERDB_NAME            = 'ensembl_web_user_db';
+  $SiteDefs::ENSEMBL_USERDB_USER            = 'ensro';
+  $SiteDefs::ENSEMBL_USERDB_HOST            = 'ensdb-userdb.internal.sanger.ac.uk';
+  $SiteDefs::ENSEMBL_USERDB_PORT            =  5305;
+  $SiteDefs::ENSEMBL_USER_COOKIE            = 'ENSEMBL_DEV_USER';
+  $SiteDefs::ENSEMBL_SESSION_COOKIE         = 'ENSEMBL_DEV_SESSION';
+  $SiteDefs::ENSEMBL_MART_ENABLED           = 0;
+  $SiteDefs::ENSEMBL_MIRRORS                = {};
+  $SiteDefs::UDC_CACHEDIR                   = '';
+
+  $SiteDefs::LUCENE_ENDPOINT                = "http://webindex-dev.internal.sanger.ac.uk:8080/sanger-web/ws/searchService";
+  $SiteDefs::LUCENE_EXT_ENDPOINT            = "http://webindex-dev.internal.sanger.ac.uk:8080/sanger-web/ws/searchExtService";
 
   $SiteDefs::ENSEMBL_WEBADMIN_ID     = 94;
   $SiteDefs::ENSEMBL_USERADMIN_ID    = 95;
