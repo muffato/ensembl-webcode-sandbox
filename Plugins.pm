934aeac6a7c3acf4111c879e67cb3834177389cf
Matthieu Muffato <muffato@ebi.ac.uk>
2017-05-08 00:31:41 +0100

    Updated list of plugins for the EBI

--- Plugins.pm
+++ Plugins.pm
@@ -5,11 +5,19 @@
 
 $SiteDefs::ENSEMBL_PLUGINS = [
   'EnsEMBL::Matt'         => $SiteDefs::ENSEMBL_SERVERROOT.'/sandbox-mm14',
-  #'EnsEMBL::Staging'      => $SiteDefs::ENSEMBL_SERVERROOT.'/sanger-plugins/staging',
-  'EnsEMBL::Widgets'      => $SiteDefs::ENSEMBL_SERVERROOT.'/public-plugins/widgets',
-  'EnsEMBL::Sanger_dev'   => $SiteDefs::ENSEMBL_SERVERROOT.'/sanger-plugins/dev',
+  'EnsEMBL::Staging'      => $SiteDefs::ENSEMBL_SERVERROOT.'/ebi-plugins/staging',
+
+  'EnsEMBL::Dev'          => $SiteDefs::ENSEMBL_SERVERROOT.'/ebi-plugins/dev',
+  'EnsEMBL::Common'       => $SiteDefs::ENSEMBL_SERVERROOT.'/ebi-plugins/common',
+  'EnsEMBL::Widgets'      => $SiteDefs::ENSEMBL_SERVERROOT.'/public-plugins/widgets', #needed for CAFE and Species Tree widgets
+  #'EnsEMBL::Solr'         => $SiteDefs::ENSEMBL_SERVERROOT.'/public-plugins/solr',
+  #'EnsEMBL::Nginx'        => $SiteDefs::ENSEMBL_SERVERROOT.'/public-plugins/nginx',
+  #'EnsEMBL::Memcached'    => $SiteDefs::ENSEMBL_SERVERROOT.'/public-plugins/memcached',
+  #'EnsEMBL::Mart'         => $SiteDefs::ENSEMBL_SERVERROOT.'/public-plugins/mart',
   'EnsEMBL::Genoverse'    => $SiteDefs::ENSEMBL_SERVERROOT.'/public-plugins/genoverse',
-  'EnsEMBL::Sanger'       => $SiteDefs::ENSEMBL_SERVERROOT.'/sanger-plugins/sanger',
+  #'EnsEMBL::Tools_hive'   => $SiteDefs::ENSEMBL_SERVERROOT.'/public-plugins/tools_hive',
+  #'EnsEMBL::Tools'        => $SiteDefs::ENSEMBL_SERVERROOT.'/public-plugins/tools',
+  #'EnsEMBL::Users'        => $SiteDefs::ENSEMBL_SERVERROOT.'/public-plugins/users',
   'EnsEMBL::Ensembl'      => $SiteDefs::ENSEMBL_SERVERROOT.'/public-plugins/ensembl',
   'EnsEMBL::Docs'         => $SiteDefs::ENSEMBL_SERVERROOT.'/public-plugins/docs',
 ];
